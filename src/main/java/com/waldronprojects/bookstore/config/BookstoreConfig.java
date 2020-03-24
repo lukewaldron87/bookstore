a4e1d9779d77a9ff38f68cabe88aa14f38450a74
Luke Waldron <lukewaldron87@gmail.com>
2020-03-24 09:51:45 +0100

    initial commit

---
+++ src/main/java/com/waldronprojects/bookstore/config/BookstoreConfig.java
@@ -0,0 +1,163 @@
+package com.waldronprojects.bookstore.config;
+
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan("com.waldronprojects.bookstore")
+public class BookstoreConfig {
+
+	@Autowired
+	private Environment env;
+	private Logger logger = Logger.getLogger(getClass().getName());
+	private boolean debug = true;
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+	
+		ComboPooledDataSource dataSource = new ComboPooledDataSource();
+		
+		try {
+			dataSource.setDriverClass("com.mysql.jdbc,Driver");
+		}catch(PropertyVetoException exc) {
+			throw new RuntimeException(exc);
+		}
+		
+		if(debug) {
+			logger.info("jdbc.url=" + env.getProperty("jdbc.url"));
+			logger.info("jdbc.user=" + env.getProperty("jdbc.user"));
+		}
+		
+		// set database connection propeties
+		dataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		dataSource.setUser(env.getProperty("jdbc.user"));
+		dataSource.setPassword(env.getProperty("jdbc.password"));
+		
+		// set connection pool props
+		dataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+		dataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+		dataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+		dataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+		
+		return dataSource;
+	}
+	
+	private Properties getHibernateProperties() {
+		
+		Properties properties = new Properties();
+		
+		properties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+		properties.setProperty("hibernate,show_sql", env.getProperty("hibernate.show_sql"));
+		
+		return properties;
+	}
+	
+	@Bean
+	public DataSource securityDataSource() {
+	
+		ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+		
+		try {
+			securityDataSource.setDriverClass("com.mysql.jdbc,Driver");
+		}catch(PropertyVetoException exc) {
+			throw new RuntimeException(exc);
+		}
+		
+		if(debug) {
+			logger.info(">>> security.jdbc.url=" + env.getProperty("security.jdbc.url"));
+			logger.info(">>> security.jdbc.user=" + env.getProperty("security.jdbc.user"));
+		}
+		
+		securityDataSource.setJdbcUrl(env.getProperty("security.jdbc.url"));
+		securityDataSource.setUser(env.getProperty("security.jdbc.user"));
+		securityDataSource.setPassword(env.getProperty("security.jdbc.password"));
+		
+		securityDataSource.setInitialPoolSize(
+				getIntProperty("security.connection.pool.initialPoolSize"));
+
+		securityDataSource.setMinPoolSize(
+				getIntProperty("security.connection.pool.minPoolSize"));
+
+		securityDataSource.setMaxPoolSize(
+				getIntProperty("security.connection.pool.maxPoolSize"));
+
+		securityDataSource.setMaxIdleTime(
+				getIntProperty("security.connection.pool.maxIdleTime"));
+		
+		return securityDataSource;
+	}
+	
+	private int getIntProperty(String propName) {
+		
+		String propVal = env.getProperty(propName);
+		
+		// now convert to int
+		int intPropVal = Integer.parseInt(propVal);
+		
+		return intPropVal;
+		
+	}
+	
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		
+		// set the properties
+		sessionFactory.setDataSource(dataSource());
+		sessionFactory.setPackagesToScan(env.getProperty("hibernate.packagesToScan"));
+		sessionFactory.setHibernateProperties(getHibernateProperties());
+		
+		return sessionFactory;
+	}
+	
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+		
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+		transactionManager.setSessionFactory(sessionFactory);
+	
+		return transactionManager;
+	}
+	
+	/*@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resurces/**")
+				.addResourceLocations("/resurces/");
+	}*/
+}
