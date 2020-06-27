eefbe78e7a7dcc9271cbe60c2e442d103bdfe5fb
Luke Waldron <lukewaldron87@gmail.com>
2020-06-27 12:17:39 +0200

    Added a User Factory for Unit Tests to use

---
+++ src/test/java/com/waldronprojects/bookstore/factory/UserEntityFactory.java
@@ -0,0 +1,9 @@
+package com.waldronprojects.bookstore.factory;
+
+import com.waldronprojects.bookstore.entity.User;
+
+public abstract class UserEntityFactory {
+	
+	public abstract User createUser(String type);
+
+}
