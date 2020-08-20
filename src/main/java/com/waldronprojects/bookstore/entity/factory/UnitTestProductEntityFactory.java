2cfaf819ccb56504df705db449ed6dae4d646718
Luke Waldron <lukewaldron87@gmail.com>
2020-08-20 11:53:51 +0200

    Refactored UnitTestProductEntityFactory and created unit tests
    - Changed UnitTestProductEntityFactory to use Map instead of switch statement
    - Created unit tests for UnitTestProductEntityFactory

--- src/main/java/com/waldronprojects/bookstore/entity/factory/UnitTestProductEntityFactory.java
+++ src/main/java/com/waldronprojects/bookstore/entity/factory/UnitTestProductEntityFactory.java
@@ -2,22 +2,27 @@ package com.waldronprojects.bookstore.entity.factory;
 
 import com.waldronprojects.bookstore.entity.Product;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class UnitTestProductEntityFactory extends ProductEntityFactory {
+
+    private static final Map<ProductType, Product> FACTORY_MAP;
+
+    static {
+        final HashMap<ProductType, Product> factoryMap = new HashMap<>();
+        factoryMap.put(ProductType.GENERIC, createGenericProduct());
+        FACTORY_MAP = Collections.unmodifiableMap(factoryMap);
+    }
 
     @Override
     public Product createProduct(ProductType productType) {
-        Product product = null;
-        switch (productType){
-            case GENERIC:
-                product = createGenericProduct();
-                break;
-            default:
-                throw new IllegalArgumentException("Argument " + productType + " strange supported");
-        }
+        Product product = FACTORY_MAP.get(productType);
         return product;
     }
 
-    private Product createGenericProduct() {
+    private static Product createGenericProduct() {
         Product product = new Product();
         product.setId(1);
         product.setProductName("genericProduct");
--- src/test/java/com/waldronprojects/bookstore/entity/factory/UnitTestProductEntityFactoryTest.java
+++ src/test/java/com/waldronprojects/bookstore/entity/factory/UnitTestProductEntityFactoryTest.java
@@ -1,7 +1,39 @@
 package com.waldronprojects.bookstore.entity.factory;
 
+import com.waldronprojects.bookstore.entity.Product;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class UnitTestProductEntityFactoryTest {
 
+    private ProductEntityFactory factory;
+    private static final String BAD_PRODUCT_TYPE = "badProductType";
+
+    @Before
+    public void setUp(){
+        factory = new UnitTestProductEntityFactory();
+    }
+
+    @Test
+    public void testCreateProductReturnsCorrectType(){
+        Product product = factory.createProduct(ProductType.GENERIC);
+        assertTrue(product instanceof Product);
+    }
+
+    @Test
+    public void testCreateProductStaticValues(){
+        Product product = factory.createProduct(ProductType.GENERIC);
+        assertEquals(product.getId(), 1);
+        assertEquals(product.getProductName(), "genericProduct");
+        assertEquals(product.getUnitPrice(), 1.1, 0.001);
+        assertEquals(product.getDiscription(), "productDescription");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateProductIllegalArgument(){
+        Product product = factory.createProduct(ProductType.valueOf(BAD_PRODUCT_TYPE));
+    }
 }
