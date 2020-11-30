a75194aea4e9b67dcd37fc54ffa9ab71d9d8d4e8
lukewaldron87 <lukewaldron87@gmail.com>
2020-11-30 12:10:26 +0100

    commit test

--- src/test/java/com/waldronprojects/bookstore/util/UnitTestRoleEntityCollectionFactory.java
+++ src/test/java/com/waldronprojects/bookstore/util/UnitTestRoleEntityCollectionFactory.java
@@ -11,6 +11,7 @@ public class UnitTestRoleEntityCollectionFactory extends RoleEntityCollectionFac
     private final Map<RoleType, Collection<Role>> FACTORY_MAP;
 
     public UnitTestRoleEntityCollectionFactory() {
+        //final Map<RoleType, Collection<Role>> factoryMap = new EnumMap<>(RoleType.class);
         final HashMap<RoleType, Collection<Role>> factoryMap = new HashMap<>();
         factoryMap.put(RoleType.ROLE_CUSTOMER, createCustomerRole());
         factoryMap.put(RoleType.ROLE_EMPLOYEE, createEmployeeRole());
